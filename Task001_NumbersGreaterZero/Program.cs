c52239149f1297a1cf2a2b111d47d1c3688f8cbe
Valentin Chudinov <valentinchudinov@gmail.com>
2023-03-02 16:18:38 +0700

    last commit

--- Task001_NumbersGreaterZero/Program.cs
+++ Task001_NumbersGreaterZero/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+
+Console.Write("Enter numbers separated by spaces: ");
+string input = Console.ReadLine();
+
+int[] numbers = ParseInput(input);
+int count = CountPositive(numbers);
+
+Console.WriteLine($"Number of positive numbers: {count}");
+Console.Write("Positive numbers: ");
+PrintPositive(numbers);
+    
+
+    static int[] ParseInput(string input) // разбивает строку ввода на отдельные числа и сохраняет их в массив.
+{  
+    string[] parts = input.Split(' ');
+    int[] numbers = new int[parts.Length];
+    for (int i = 0; i < parts.Length; i++)
+    {
+        numbers[i] = int.Parse(parts[i]);
+    }
+    return numbers;
+}
+
+static int CountPositive(int[] numbers) // считает количество положительных чисел в массиве. 
+{
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0)        
+            count++;       
+    }
+    return count;
+}
+
+static void PrintPositive(int[] numbers) // выводит на экран все положительные числа из массива.
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0)        
+            Console.Write(numbers[i] + " ");        
+    }
+}
---
+++ Task002_IntersectionPoint/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем
+
+Console.Clear();
+
+static void Main(string[] args)
+{
+    Console.Write("Введите значение k1: ");
+    double k1 = double.Parse(Console.ReadLine());
+
+    Console.Write("Введите значение b1: ");
+    double b1 = double.Parse(Console.ReadLine());
+
+    Console.Write("Введите значение k2: ");
+    double k2 = double.Parse(Console.ReadLine());
+
+    Console.Write("Введите значение b2: ");
+    double b2 = double.Parse(Console.ReadLine());
+
+    double x = FindIntersection(k1, b1, k2, b2);
+    double y = k1 * x + b1;
+
+    Console.WriteLine($"Точка пересечения: ({x}, {y})");
+}
+
+static double FindIntersection(double k1, double b1, double k2, double b2)
+{
+    return (b2 - b1) / (k1 - k2);
+}
